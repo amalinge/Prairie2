71c2d3c068746d2322ee0b1953284838c6871948
Agnès <simplon@simplon-Latitude-E6410>
2017-04-19 16:58:48 +0200

    comit fct

--- Java_prairie2/src/Main.java
+++ Java_prairie2/src/Main.java
@@ -6,7 +6,8 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Blablabla3..");
+		// System.out.println("Blabla3..");
+		bonjour ();
 	}
 
 }
